c6b61a810314e153b9091a825d7af2ce6e84937e
Govert Verschuur <govert.verschuur@gmail.com>
2015-08-20 00:40:47 +0200

    app (php executable file), composer and command stub templates

---
+++ app/templates/src/App/Command/StubCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace <%=PHP_NAMESPACE%>\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class <%=PHP_CLASSNAME%> extends Command {
+
+	protected function configure() {
+        $this
+			->setName('<%=COMMAND_NAMESPACE%>:<%=COMMAND_NAME%>')
+            ->setDescription('<%=COMMAND_DESCRIPTION%>')
+            ->setHelp('<%=COMMAND_HELP%>');
+		
+		// extra command line arguments and options go here.          
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$output->writeln('<%=COMMAND_NAMESPACE%>:<%=COMMAND_NAME%> called');
+        // your command code goes here.
+    }
+}
