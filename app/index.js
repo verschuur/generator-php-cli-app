2a6d2fb4b229ccf19d98b81c20a6b75938a98c16
Govert Verschuur <govert.verschuur@gmail.com>
2015-08-20 00:37:58 +0200

    commented away for now

--- app/index.js
+++ app/index.js
@@ -20,6 +20,29 @@ module.exports = yeoman.generators.Base.extend({
     };
   },
 
+  // prompting: function () {    
+  //   var done = this.async();
+ 
+  //   // Have Yeoman greet the user.
+  //   this.log(yosay(
+  //     'Welcome to the slick ' + chalk.red('SymfonyConsoleScaffolding') + ' generator!'
+  //   ));
+
+  //   var prompts = [{
+  //     type: 'confirm',
+  //     name: 'someOption',
+  //     message: 'Would you like to enable this option?',
+  //     default: true
+  //   }];
+
+  //   this.prompt(prompts, function (props) {
+  //     this.props = props;
+  //     // To access props later use this.props.someOption;
+
+  //     done();
+  //   }.bind(this));
+  // },
+
   writing: {
     app: function () {
       this.fs.copy(
