83f4f2e4e3f7cd22a93996e68590ac8b528d80ee
Govert Verschuur <govert.verschuur@gmail.com>
2015-08-20 00:37:17 +0200

    define stub settings/replace values

--- app/index.js
+++ app/index.js
@@ -7,27 +7,17 @@ var path = require('path');
 var cc = require('change-case');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the slick ' + chalk.red('SymfonyConsoleScaffolding') + ' generator!'
-    ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
-
-      done();
-    }.bind(this));
+  
+  settings: function () {
+    this.settings = {
+      phpNamespace: cc.pascalCase(this.appname),
+      phpClassName: cc.pascalCase(this.appname) + 'Command', // PHP file and classname (with the 'Command' suffix)
+      commandNamespace: cc.paramCase(this.appname), // command namespace, e.g. foobar:example (foobar)
+      commandName: cc.paramCase('example'), // command name, e.g. foobar:example (example)
+      appName: cc.titleCase(this.appname), // app name used in composer and app settings
+      appVersion: '1.0', // app version used in composer and app settings
+      entrypointName: cc.paramCase(this.appname), // app entry point / php executable
+    };
   },
 
   writing: {
