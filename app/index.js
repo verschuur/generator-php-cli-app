818121bff535cfc496207c1ec17bc496111eb2f5
Govert Verschuur <govert.verschuur@gmail.com>
2015-08-21 00:05:36 +0200

    more cleanups

--- app/index.js
+++ app/index.js
@@ -35,22 +35,21 @@ module.exports = yeoman.generators.Base.extend({
    * Running context priorities
    * http://yeoman.io/authoring/running-context.html
    */
-  initializing: {
-    settings: function () {
-      this.settings = {
-        phpNamespace: cc.pascalCase(this.appname),
-        phpClassName: cc.pascalCase(this.appname) + 'Command', // PHP file and classname (with the 'Command' suffix)
-        commandNamespace: cc.paramCase(this.appname), // command namespace, e.g. foobar:example (foobar)
-        commandName: cc.paramCase('example'), // command name, e.g. foobar:example (example)
-        appName: cc.titleCase(this.appname), // app name used in composer and app settings
-        appVersion: '1.0', // app version used in composer and app settings
-        entrypointName: cc.paramCase(this.appname), // app entry point / php executable
-      };
+  initializing: function () {    
+    this.settings = {
+      phpNamespace: cc.pascalCase(this.appname),
+      phpClassName: cc.pascalCase(this.appname) + 'Command', // PHP file and classname (with the 'Command' suffix)
+      commandNamespace: cc.paramCase(this.appname), // command namespace, e.g. foobar:example (foobar)
+      commandName: cc.paramCase('example'), // command name, e.g. foobar:example (example)
+      appName: cc.titleCase(this.appname), // app name used in composer and app settings
+      appVersion: '1.0', // app version used in composer and app settings
+      entrypointName: cc.paramCase(this.appname), // app entry point / php executable
+    };
 
-      if(this.options.settings) {
-        this._showSettings();
-        return;
-      }
+    if(this.options.settings) {
+      this._showSettings();
+      return;
+    }
   },
 
   // prompting: function () {    
@@ -76,64 +75,52 @@ module.exports = yeoman.generators.Base.extend({
   //   }.bind(this));
   // },
 
-  
+  configuring: function () {    
+    this.fs.copyTpl(
+      this.templatePath('_composer.json'),
+      this.destinationPath('composer.json'), 
+      { 
+        PROJECT_NAME: this.settings.entrypointName,
+        PHP_NAMESPACE: this.settings.phpNamespace
+      }
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('_README.md'),
+      this.destinationPath('README.md'), 
+      { 
+        APPLICATION_NAME: this.settings.appName,
+        COMMAND_NAMESPACE: this.settings.commandNamespace,
+        COMMAND_NAME: this.settings.commandName,
+        PROJECT_NAME: this.settings.entrypointName
+      }
+    );
   },
 
-  configuring: {
-    app: function () {
-      this.fs.copyTpl(
-        this.templatePath('_composer.json'),
-        this.destinationPath('composer.json'), 
-        { 
-          PROJECT_NAME: this.settings.entrypointName,
-          PHP_NAMESPACE: this.settings.phpNamespace
-        }
-      );
+  writing: function () {    
+    this.fs.copyTpl(
+      this.templatePath('_app'),
+      this.destinationPath(this.settings.entrypointName), 
+      { 
+        PHP_NAMESPACE: this.settings.phpNamespace,
+        APPLICATION_NAME: this.settings.appName,
+        APPLICATION_VERSION: this.settings.appVersion,
+        PHP_CLASSNAME: this.settings.phpClassName
+      }
+    );
 
-      this.fs.copyTpl(
-        this.templatePath('_README.md'),
-        this.destinationPath('README.md'), 
-        { 
-          APPLICATION_NAME: this.settings.appName,
-          COMMAND_NAMESPACE: this.settings.commandNamespace,
-          COMMAND_NAME: this.settings.commandName,
-          PROJECT_NAME: this.settings.entrypointName
-        }
-      );
-
-    }
-  },
-
-  writing: {
-    app: function () {
-      this.fs.copyTpl(
-        this.templatePath('_app'),
-        this.destinationPath(this.settings.entrypointName), 
-        { 
-          PHP_NAMESPACE: this.settings.phpNamespace,
-          APPLICATION_NAME: this.settings.appName,
-          APPLICATION_VERSION: this.settings.appVersion,
-          PHP_CLASSNAME: this.settings.phpClassName
-        }
-      );
-
-      this.fs.copyTpl(
-        this.templatePath('src/App/Command/StubCommand.php'),
-        this.destinationPath('src/' + this.settings.phpNamespace + '/Command/' + this.settings.phpClassName + '.php'), 
-        { 
-          PHP_NAMESPACE: this.settings.phpNamespace,
-          PHP_CLASSNAME: this.settings.phpClassName,
-          COMMAND_NAMESPACE: this.settings.commandNamespace,
-          COMMAND_NAME: this.settings.commandName,
-          COMMAND_DESCRIPTION: 'The command description goes here',
-          COMMAND_HELP: 'The command help text goes here'
-        }
-      );
-    },
-
-    projectfiles: function () {
-      
-    }
+    this.fs.copyTpl(
+      this.templatePath('src/App/Command/StubCommand.php'),
+      this.destinationPath('src/' + this.settings.phpNamespace + '/Command/' + this.settings.phpClassName + '.php'), 
+      { 
+        PHP_NAMESPACE: this.settings.phpNamespace,
+        PHP_CLASSNAME: this.settings.phpClassName,
+        COMMAND_NAMESPACE: this.settings.commandNamespace,
+        COMMAND_NAME: this.settings.commandName,
+        COMMAND_DESCRIPTION: 'The command description goes here',
+        COMMAND_HELP: 'The command help text goes here'
+      }
+    );
   },
 
   install: function () {
@@ -144,6 +131,6 @@ module.exports = yeoman.generators.Base.extend({
           this.log.write(chalk.green('php ' + this.settings.entrypointName + '\n'));
         }
       }.bind(this));    
-  }
+  },
 
 });
